8f9f96c9e8f6a48335db72eaeea861206981c637
chinyuting <s86052915@gmail.com>
2024-07-24 00:03:59 +0800

    edit validator about auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,41 @@
 const jwt = require('jsonwebtoken');
+const { header, validationResult } = require('express-validator');
 const secretKey = process.env.JWT_SECRET_KEY;
 
-const authenticateJWT = (req, res, next) => {
-  // 取得header中的authorization
-  const authHeader = req.headers.authorization;
-  if (authHeader) {
-    // Bearer取出token
-    const token = authHeader.split(' ')[1];
-    // 驗證Bearer是否有效
-    jwt.verify(token, `${secretKey}`, (err, user) => {
-      if (err) {
-        return res.status(403).json({ errors: [{ msg: '未正確授權', param: 'token' }] });
-      }
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.status(403).json({ errors: [{ msg: '未正確授權', param: 'token' }] });
+const authenticate = [
+  // 验证是否带有 Bearer 且格式正确
+  header('Authorization')
+    .exists().withMessage('未正確授權')
+    .matches(/^Bearer\s[\w-]+\.[\w-]+\.[\w-]+$/).withMessage('未正確授權'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+
+  (req, res, next) => {
+    // 取得header中的authorization
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      // Bearer取出token
+      const token = authHeader.split(' ')[1];
+      // 驗證Bearer是否有效
+      jwt.verify(token, `${secretKey}`, (err, user) => {
+        if (err) {
+          return res.status(403).json({ errors: [{ msg: '未正確授權', param: 'token' }] });
+        }
+        req.user = user;
+        next();
+      });
+    } else {
+      return res.status(403).json({ errors: [{ msg: '未正確授權', param: 'token' }] });
+    }
   }
-};
+];
 
 module.exports = {
-  authenticateJWT
+  authenticate
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,21 +6,20 @@ const todosController = require('../controllers/todosController');
 
 const { usersValidate } = require('../validators/usersValidator');
 const { registerUsersValidator } = require('../validators/registerUsersValidator');
-const { authValidator } = require('..//validators/authValidator');
 const { todoIdValidator } = require('..//validators/todoIdValidator');
 const { todoContentValidator } = require('..//validators/todoContentValidator');
 
-const { authenticateJWT } = require('../middleware/auth');
+const { authenticate } = require('../middleware/auth');
 
 // user相關
 router.post('/users', registerUsersValidator,userController.registerUsers);
 router.post('/users/sign_in', usersValidate,userController.signInUsers);
 
 // todos相關
-router.get('/todos',authValidator, authenticateJWT, todoContentValidator, todosController.getTodos);
-router.post('/todos',authValidator, authenticateJWT, todoContentValidator, todosController.addTodo);
-router.put('/todos/complete/:todoId',authValidator ,authenticateJWT ,todoIdValidator , todosController.toggleTodoAsCompleted);
-router.put('/todos/edit/:todoId',todoIdValidator ,authValidator, authenticateJWT, todosController.editTodoContent);
-router.delete('/todos/:todoId',todoIdValidator ,authValidator, authenticateJWT, todosController.removeTodo);
+router.get('/todos', authenticate, todoContentValidator, todosController.getTodos);
+router.post('/todos',authenticate, todoContentValidator, todosController.addTodo);
+router.put('/todos/complete/:todoId',authenticate ,todoIdValidator , todosController.toggleTodoAsCompleted);
+router.put('/todos/edit/:todoId',todoIdValidator ,authenticate, todosController.editTodoContent);
+router.delete('/todos/:todoId',todoIdValidator ,authenticate, todosController.removeTodo);
 
 module.exports = router;
